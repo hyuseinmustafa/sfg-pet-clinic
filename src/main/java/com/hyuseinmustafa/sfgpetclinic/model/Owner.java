f67aa5ea53aff840dd5e0f85a1f8ba124f289dfd
Hyusein Mustafa <hyseinmustafa@gmail.com>
2020-12-30 19:33:24 +0000

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/hyuseinmustafa/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.hyuseinmustafa.sfgpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/hyuseinmustafa/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.hyuseinmustafa.sfgpetclinic.model;
+
+public class Vet extends Person{
+}
