ec8e7e8214b73aa8e1a341b8e0dc720e3bcc24cb
Hyusein Mustafa <hyseinmustafa@gmail.com>
2020-12-31 07:55:13 +0000

    Implementing Interfaces for Services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/hyuseinmustafa/sfgpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package com.hyuseinmustafa.sfgpetclinic.services;
+
+import com.hyuseinmustafa.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
