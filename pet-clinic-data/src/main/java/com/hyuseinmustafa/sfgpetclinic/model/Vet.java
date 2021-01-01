91e52b9cd75210f6f796f6f78f0083aca48e3794
Hyusein Mustafa <hyseinmustafa@gmail.com>
2021-01-01 13:22:29 +0000

    Added specialities, added to vet. Closes #27

--- pet-clinic-data/src/main/java/com/hyuseinmustafa/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/hyuseinmustafa/sfgpetclinic/model/Vet.java
@@ -1,4 +1,15 @@
 package com.hyuseinmustafa.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+    private Set<Spetiality> spetialities;
+
+    public Set<Spetiality> getSpetialities() {
+        return spetialities;
+    }
+
+    public void setSpetialities(Set<Spetiality> spetialities) {
+        this.spetialities = spetialities;
+    }
 }
